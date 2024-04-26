7bb86aca1cebccf4388cd344c7a2184e5a28b36a
Jim Ruppert <ruppej@sulaco.local>
2024-04-26 15:09:08 -0500

    Day 1 A

---
+++ day1/print_modifiers.py
@@ -0,0 +1,7 @@
+print ('She said: "Hello" and then left.')
+
+print ("She said: \"Hello\" and then left.")
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
